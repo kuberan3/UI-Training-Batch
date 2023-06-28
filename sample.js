3d139466e6ef83b924042c9d725471da1f48a9e1
kuberan3 <kuberanvenkatesh3@gmail.com>
2023-06-28 17:29:27 +0530

    js-first-day

---
+++ sample.js
@@ -0,0 +1,40 @@
+
+// var c = 1.2343;
+// console.log(c.toFixed());
+
+// console.log(5 - 'c') 
+
+// var a=10;
+
+// console.log(typeof(a.toString()));
+
+// console.log(3 * 'a')
+
+// var doubleQuoted = "This Is New \"Java Script\" Page";
+
+
+// var doubleQuoted = "This Is New \'Java Script\' Page";
+
+
+// var doubleQuoted = "This Is New Java Script Page";
+
+
+// console.log(doubleQuoted.toLowerCase())
+
+// console.log(doubleQuoted.toUpperCase())
+
+// console.log(doubleQuoted.length)
+
+// console.log(doubleQuoted.indexOf('Java'))
+
+// console.log(doubleQuoted.slice(7))
+
+// console.log(doubleQuoted.lastIndexOf('a'))
+
+// console.log('kuberan'+' '+'venkatesh')
+
+// var firstName='kuberan'
+// var secondName='venkatesh'
+
+// console.log(firstName.concat(" ",secondName))
+
